ffd8e935cdfbd04b1464fb8558edafb1fa169e44
Komal Hasija <komalhasija4020@gmail.com>
2025-07-05 15:37:43 +0530

    finn

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 require('dotenv').config();
 const axios = require('axios');
 const PDFDocument = require('pdfkit');
-const cors = require('cors');
+
 
 const allowedOrigins = [
   'http://localhost:5173',
